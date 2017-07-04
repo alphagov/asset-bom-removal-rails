8ef4bd1906a66e2c6163f4994fca1e863a2e2327
Murray Steele <muz@h-lame.com>
2017-07-04 15:38:46 +0000

    Initial boilerplate for gem
    Generated from `bundler gem` and then tweaked to add README and other
    bits specific to gov.uk infrastructure.

---
+++ lib/asset_bom_removal/rails/railtie.rb
@@ -0,0 +1,8 @@
+require "govuk_sidekiq/sidekiq_initializer"
+
+module AssetBomRemoval
+  module Rails
+    class Railtie < ::Rails::Railtie
+    end
+  end
+end
---
+++ lib/asset_bom_removal/rails/version.rb
@@ -0,0 +1,5 @@
+module AssetBomRemoval
+  module Rails
+    VERSION = '1.0.0'.freeze
+  end
+end
